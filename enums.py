ffd18b019fd67cc5fa0459a6078c9dd7f8521fae
Karbo-101 <kaiboettinger@web.de>
2024-02-17 20:39:17 +0100

    enums & Konstanten

---
+++ enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+ 
+class Klasse(Enum):
+    Krieger = 1
+    Kleriker = 2
+    Magier = 3
+    Druide = 4
+
+
+class Rasse(Enum):
+    Mensch = 1
+    Elf = 2
+    Tiefling = 3
+    Druide = 4
