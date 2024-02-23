1cd9f7527bf0f814bcf30b44ca75c8e9770fa10c
Karbo-101 <kaiboettinger@web.de>
2024-02-23 20:52:41 +0100

    Konstanten erweitert

--- constants.py
+++ constants.py
@@ -4,3 +4,9 @@ MIN_HEALTH_START = 50
 # Items
 POTION_HP_REGAIN = 20
 HIGH_POTION_HP_REGAIN = 50 
+
+# NPCs
+npcs = ['Händler', 'Bürgermeister Fallenberg', 'Carlo der Schmied']
+
+# Orte
+places = ['Tronjheim', 'Rabenwald', 'Berg der Wahrheit']
